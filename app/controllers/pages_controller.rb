ca8d29d53f342509febb32a7b30b0f56a7537574
Duarte Lucas <duarteprista@gmail.com>
2017-03-07 16:07:23 +0000

    search own list by genre

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,18 +8,31 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-  @lists = List.all
+    @lists = List.all
+    @movie_titles = []
+    @movies_out = []
+
     search_value = params[:search_value]
-    if !search_value.nil?
-      #@search_result = Tmdb::Genre.movie_list
-      #@search_result = Tmdb::Search.keyword(search_value).results
-      Tmdb::Genre.movie_list.each do |genre|
-          if genre.name.downcase == search_value.downcase
-            genre_movies = Tmdb::Genre.movies(genre.id)
-            @search_result = genre_movies.results
-            puts @search_result
-          end
-       end
-    end
+      if !search_value.nil?
+        #@search_result = Tmdb::Genre.movie_list
+        #@search_result = Tmdb::Search.keyword(search_value).results
+        Tmdb::Genre.movie_list.each do |genre|
+            if genre.name.downcase == search_value.downcase
+              genre_movies = Tmdb::Genre.movies(genre.id)
+              @search_result = genre_movies.results
+
+
+              @search_result.flatten.each do |m| @movie_titles << m.title end
+              # @movie_titles is an array of the movie titles from the API
+
+              @movies_out = List.has_movies?(@movie_titles)
+              # @movies_out is an array of movie obj IF the searched movies are in any list
+
+            end
+         end
+      end
+
+      @movies_out
   end
 end
+
